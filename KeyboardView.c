1c1dfe56b0416639cd16ce38e70cda49da897877
Pierce Forrest England <pfengland@gmail.com>
2017-02-23 08:52:08 -0500

    showing up down arrows on keyboard

--- KeyboardView.c
+++ KeyboardView.c
@@ -35,16 +35,30 @@ void KeyboardView_draw(KeyboardView *k, SDL_Surface *screen, int x, int y) {
      // pixel padding for squares and numbers
      int padding = 1;
 
+     SDL_Color color={0,0,0};
+     SDL_Surface *text_surface;
+     char numberText[100];
+
      // draw the initial scrollbar box
      // define the rect to draw it on
-     
      SDL_Rect um = {x: x + padding, y: y + padding,
 		    w: keySize - padding * 2, h: keySize - padding * 2};
      SDL_FillRect(screen, &um, SDL_MapRGB(screen->format, 100, 100, 100));
+
+     if(!(text_surface=TTF_RenderText_Solid(font,"<",color))) {
+	  //handle error here, perhaps print TTF_GetError at least
+     } else {
+	  um.x += padding;
+	  um.y += padding;
+	  SDL_BlitSurface(text_surface,NULL,screen,&um);
+	  //perhaps we can reuse it, but I assume not for simplicity.
+	  SDL_FreeSurface(text_surface);
+     }
+     
      keyX += keySize;
      
      for (int i=0; i<MeowKeyboard_noteCount; i++) {
-	  if (keyX > screen->w) break;
+	  if (keyX > screen->w - keySize) break;
 	  um.x = keyX;
 	  um.y = y + padding;
 	  um.w = keySize - padding * 2;
@@ -55,9 +69,6 @@ void KeyboardView_draw(KeyboardView *k, SDL_Surface *screen, int x, int y) {
 	       SDL_FillRect(screen, &um, SDL_MapRGB(screen->format, 0, 200, 0));
 	  }
 
-	  SDL_Color color={0,0,0};
-	  SDL_Surface *text_surface;
-	  char numberText[100];
 	  sprintf(numberText, "%d", i);
 	  if(!(text_surface=TTF_RenderText_Solid(font,numberText,color))) {
 	       //handle error here, perhaps print TTF_GetError at least
@@ -71,6 +82,26 @@ void KeyboardView_draw(KeyboardView *k, SDL_Surface *screen, int x, int y) {
 	  
 	  keyX += keySize;
      }
+
+     // draw the final scrollbar box
+     // define the rect to draw it on
+     um.x = screen->w - keySize + padding;
+     um.y = y + padding;
+     um.w = keySize - padding * 2;
+     um.h = keySize - padding * 2;
+     
+     SDL_FillRect(screen, &um, SDL_MapRGB(screen->format, 100, 100, 100));
+
+     if(!(text_surface=TTF_RenderText_Solid(font,">",color))) {
+	  //handle error here, perhaps print TTF_GetError at least
+     } else {
+	  um.x += padding;
+	  um.y += padding;
+	  SDL_BlitSurface(text_surface,NULL,screen,&um);
+	  //perhaps we can reuse it, but I assume not for simplicity.
+	  SDL_FreeSurface(text_surface);
+     }
+
 
      TTF_CloseFont(font);
 }
